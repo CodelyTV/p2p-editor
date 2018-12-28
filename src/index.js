deba4df060a450c6a740e4e294721df52c13a432
Daniel Santamaría <santaka87@gmail.com>
2018-12-28 10:31:42 +0100

    setup hypercore

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
-console.log('webpack working');
+const hypercore = require('hypercore')
+const ram = require('random-access-memory')
+
+const feed = hypercore(function (filename) {
+  return ram()
+})
+
+feed.on('ready', () => {
+    console.log(feed.key.toString('hex'))
+
+    feed.createReadStream({live: true}).on('data', (data) => {
+        console.log(data.toString())
+    })
+})
+
+var textarea = document.getElementById('textarea')
+
+textarea.addEventListener('keydown', (e) => {
+    feed.append(e.key)
+})
