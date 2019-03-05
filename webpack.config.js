6fd612a7013b196d42c9aa19cbf38870b0f380e9
Daniel Santamaría <santaka87@gmail.com>
2019-03-05 07:17:40 +0100

    setup babel

--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,20 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist')
   },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  },
   plugins: [
     new Dotenv(),
     new CopyWebpackPlugin([
