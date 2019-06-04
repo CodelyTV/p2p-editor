2a8d4f2c27ee8b81c378e9a996e4bc233ec317cb
Daniel Santamaría <santaka87@gmail.com>
2019-06-04 07:15:33 +0200

    add close button to user list panel

--- src/components/user-list/UserListComponent.jsx
+++ src/components/user-list/UserListComponent.jsx
@@ -1,15 +1,41 @@
-import React from 'react'
+import React, { Component } from 'react'
 import PropTypes from "prop-types";
 import UserComponent from './UserListItemComponent';
 
-const UserListComponent = ({users}) => (
-  <div className="user-list">
-    <h2 className="user-list__title">Users</h2>
-    <ul className="user-list__container">
-      {users.map((user) => <UserComponent key={user.id} user={user} />)}
-    </ul>
-  </div>
-)
+class UserListComponent extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      active: true
+    }
+  }
+
+  render() {
+    const { users } = this.props
+
+    return (
+      <div className={`user-list ${this.activeClassName()}`}>
+        <span className="user-list__close-btn" onClick={() => {this.close()}}>-</span>
+        <h2 className="user-list__title">Users</h2>
+        <ul className="user-list__container">
+          {users.map((user) => <UserComponent key={user.id} user={user}/>)}
+        </ul>
+      </div>
+    )
+  }
+
+  close() {
+    this.setState((state) => ({
+      ...state,
+      active: false
+    }))
+  }
+
+  activeClassName() {
+    return this.state.active ? 'user-list--active' : ''
+  }
+}
 
 UserListComponent.propTypes = {
   users: PropTypes.arrayOf(PropTypes.shape({
