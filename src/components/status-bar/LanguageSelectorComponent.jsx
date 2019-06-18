0ca336b562e27264f060dfaf9a599d18beebc041
Daniel Santamaría <santaka87@gmail.com>
2019-06-18 07:24:50 +0200

    set ace editor language

--- src/components/status-bar/LanguageSelectorComponent.jsx
+++ src/components/status-bar/LanguageSelectorComponent.jsx
@@ -1,21 +1,28 @@
 import React, { Component } from 'react'
+import ace from 'ace-builds'
 import editorLanguages from './editorLanguages'
 
 class LanguageSelectorComponent extends Component {
   constructor(props) {
     super(props)
+    this.aceEditor = ace.edit("editor")
 
     this.state = {
+      selectedLanguage: 'javascript',
       languages: editorLanguages
     }
   }
 
   render() {
     return (
-      <select>
+      <select onChange={(e) => {this.selectLanguage(e.target.value)}}>
         {this.state.languages.map(({id, name}) => <option key={id} value={id}>{name}</option>)}
       </select>
     )
+  }
+
+  selectLanguage(languageId) {
+    this.aceEditor.session.setMode(`ace/mode/${languageId}`)
   }
 }
 
